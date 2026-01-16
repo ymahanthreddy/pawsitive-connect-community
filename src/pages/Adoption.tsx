162f850da5bc2d7db2864b072e954f52f4c286a1
ymahanthreddy <102535321+ymahanthreddy@users.noreply.github.com>
2026-01-16 20:37:27 +0530

    Update Adoption.tsx

--- src/pages/Adoption.tsx
+++ src/pages/Adoption.tsx
@@ -120,6 +120,17 @@ const Adoption = () => {
                       Learn More
                     </Link>
                   </Button>
+                  <Button
+                      asChild
+                      variant="ghost"
+                      size="sm"
+                      className="w-full mt-2 text-primary"
+                  >
+                     <Link to="/adoption/details">
+                     Learn More About Adoption Process
+                     </Link>
+                  </Button>
+
                 </div>
               </motion.div>
             ))}
